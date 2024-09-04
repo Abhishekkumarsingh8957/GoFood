4b1fb32944e48ba72b180f9118ca5927405fc14c
Abhishekkumarsingh8957 <97937542+Abhishekkumarsingh8957@users.noreply.github.com>
2024-09-05 01:57:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://gofood-frontend-my1b.onrender.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
