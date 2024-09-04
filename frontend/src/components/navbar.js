6447cb0217e3265af1bccda2fc45a5607117a309
Abhishekkumarsingh8957 <97937542+Abhishekkumarsingh8957@users.noreply.github.com>
2024-09-05 01:18:11 +0530

    Update navbar.js

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,5 +1,4 @@
 import React ,{useState}from "react";
-import Badge from "@material-ui/core/Badge";
 import { Link , useNavigate } from "react-router-dom";
 import Cart from "../screens/Cart";
 import Modal from "../Modal";
